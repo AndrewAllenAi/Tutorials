0f504efac4135bd1b0dc9ad71e4574ff90ba8743
AndrewAllenAi <AndrewAllenAi@outlook.com>
2026-04-02 18:07:48 -0500

    Create follow_links.py

---
+++ follow_links.py
@@ -0,0 +1,24 @@
+# Following links
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl # defaults to certificate verification and most secure protocol (now TLS)
+
+# Ignore SSL/TLS certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = 'http://py4e-data.dr-chuck.net/known_by_Averon.html'
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+
+for i in range(6):
+    url = tags[17].get('href', None)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    print(tags[17].text)
